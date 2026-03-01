f184ff629ab8d11ff181c419853050a2184c71ce
Talha Muhammet Basak <talhabskist61@hotmail.com>
2026-03-01 22:09:22 +0300

    Add increment logic and refactor element naming

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.getElementById("count").innerText = 5
+let countElement = document.getElementById("count-element")
+let count = 0
+
+function increment() {
+    count = count + 1
+    console.log(count)
+    countElement.innerText = count
+}
