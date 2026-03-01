12197a671fa2861f289013edcba8d537f092350c
Talha Muhammet Basak <talhabskist61@hotmail.com>
2026-03-02 01:23:47 +0300

    Add save button and basic save function

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ let countElement = document.getElementById("count-element")
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     console.log(count)
     countElement.innerText = count
 }
+
+function save(){
+    console.log(count)
+}
+
